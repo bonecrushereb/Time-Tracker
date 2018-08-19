2b18de64ed38da0abde66fbf3216c790be2acf86
bonecrushereb <bonecrushereb@gmail.com>
2018-08-19 00:29:47 -0700

    add update to projects

--- project.php
+++ project.php
@@ -3,6 +3,11 @@ require 'inc/functions.php';
 
 $pageTitle = "Project | Time Tracker";
 $page = "projects";
+$title = $category = '';
+
+if (isset($_GET['id'])) {
+    list($project_id, $title, $category) = get_project(filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT));
+}
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $title = trim(filter_input(INPUT_POST,'title', FILTER_SANITIZE_STRING));
@@ -26,7 +31,13 @@ include 'inc/header.php';
 <div class="section page">
     <div class="col-container page-container">
         <div class="col col-70-md col-60-lg col-center">
-            <h1 class="actions-header">Add Project</h1>
+            <h1 class="actions-header"><?php
+            if (!empty($project_id)) {
+                echo 'Update';
+            } else {
+                echo 'Add';
+            }
+            ?> Project</h1>
             <?php 
             if (isset($error_message)) {
                 echo "<p class='message'>$error_message</p>";
@@ -36,18 +47,35 @@ include 'inc/header.php';
                 <table>
                     <tr>
                         <th><label for="title">Title<span class="required">*</span></label></th>
-                        <td><input type="text" id="title" name="title" value="" /></td>
+                        <td><input type="text" id="title" name="title" value="<?php echo $title; ?>" /></td>
                     </tr>
                     <tr>
                         <th><label for="category">Category<span class="required">*</span></label></th>
                         <td><select id="category" name="category">
                                 <option value="">Select One</option>
-                                <option value="Billable">Billable</option>
-                                <option value="Charity">Charity</option>
-                                <option value="Personal">Personal</option>
+                                <option value="Billable"<?php
+                                if ($category == "Billable") {
+                                    echo ' selected';
+                                }
+                                ?>>Billable</option>
+                                <option value="Charity"<?php
+                                if ($category == "Charity") {
+                                    echo ' selected';
+                                }
+                                ?>>Charity</option>
+                                <option value="Personal"<?php
+                                if ($category == "Personal") {
+                                    echo ' selected';
+                                }
+                                ?>>Personal</option>
                         </select></td>
                     </tr>
                 </table>
+                <?php
+                if(!empty($project_id)) {
+                    echo '<input type="hidden" name="id" value="' . $project_id . '">';
+                } 
+                ?>
                 <input class="button button--primary button--topic-php" type="submit" value="Submit" />
             </form>
         </div>
